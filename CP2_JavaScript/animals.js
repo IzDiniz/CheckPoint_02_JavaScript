0e35d7328108bde95003b7ade7a42c0a4382c3d0
IzDiniz <116819072+IzDiniz@users.noreply.github.com>
2023-10-28 10:26:33 -0300

    Add files via upload

---
+++ CP2_JavaScript/animals.js
@@ -0,0 +1,7 @@
+const animals = ["macaco", "tucano", "elefante", "pavão", "hipopotamo", "leão"];
+
+if (animals.includes("leão")){
+    console.log("Leão está em animais");
+} else{
+    console.log("Leão não está em animais");
+}
---
+++ CP2_JavaScript/filmes.js
@@ -0,0 +1,4 @@
+const my3FavoriteTVShows = ["Star Wars", "Rebels", "Clone Wars"]
+const i = my3FavoriteTVShows.length === 3
+console.log(i);
+
